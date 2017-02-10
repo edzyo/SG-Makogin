750b0f99165e92a720f0082649e577dcdaabd53e
Mykola <makogin1995@gmail.com>
2017-02-10 15:17:52 +0200

    [+] add seed user

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,6 @@
 #
 # Examples:
 #
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+  # cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+  #  Mayor.create(name: 'Emanuel', city: cities.first)
+    User.create(email: 'user@sg.com', password: '111111')
